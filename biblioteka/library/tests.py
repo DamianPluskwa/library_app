7aae03a00b4944831b7d7445954d3dfeb5cb449c
Damian <pluskwadamian@interia.pl>
2021-06-12 15:48:52 +0200

    testy factoryBoy

--- biblioteka/library/tests.py
+++ biblioteka/library/tests.py
@@ -3,6 +3,9 @@ from django.test import TestCase
 # Create your tests here.
 from django.urls import reverse
 
+from .factories import BookFactory
+from .models import Book
+
 
 class TestViewsRentedList(TestCase):
     def test_(self):
@@ -24,3 +27,38 @@ class TestViewsRentedList(TestCase):
         response = self.client.get(reverse('library:rented_list'))
         self.assertEqual(response.status_code, 200)
 
+
+class TestBookModel(TestCase):
+    def test_book_available_true(self):
+        new_book = Book(book_title="", book_author="", book_category="", publication_date=1900)
+        new_book.save()
+        copies_new_book = new_book.copiesofbooks_set.create(copies=2, available=2)
+        copies_new_book.save()
+
+        self.assertIs(new_book.book_available, True)
+
+    def test_book_available_false(self):
+        new_book = Book(book_title="", book_author="", book_category="", publication_date=1900)
+        new_book.save()
+        copies_new_book = new_book.copiesofbooks_set.create(copies=2, available=0)
+        copies_new_book.save()
+
+        self.assertIs(new_book.book_available, False)
+
+
+class TestBookModelFixtures(TestCase):
+    fixtures = ["library/fixtures/books.json"]
+
+    def test_get_all_books(self):
+        self.assertEqual(Book.objects.count(), 7)
+        self.assertEqual(Book.objects.filter(book_title__contains="Harry Potter").first().id, 3)
+
+    def test_get_book(self):
+        book = BookFactory()
+        self.assertEqual(book.book_title, "Title 0")
+        book_2 = BookFactory()
+        self.assertEqual(book_2.book_title, "Title 1")
+
+    def test_get_ten_books(self):
+        books = [BookFactory() for _ in range(10)]
+        self.assertEqual(books[1].book_title, "Title 3")
