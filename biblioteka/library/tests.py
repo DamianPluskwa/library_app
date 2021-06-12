411348151c9c024302f668e3294f4cbcb2ebd9ed
Damian <pluskwadamian@interia.pl>
2021-06-12 14:15:20 +0200

    więcej testó i naprawa błędu vidoku available_book_list

--- biblioteka/library/tests.py
+++ biblioteka/library/tests.py
@@ -5,8 +5,22 @@ from django.urls import reverse
 
 
 class TestViewsRentedList(TestCase):
-    def test(self):
+    def test_(self):
         response = self.client.get(reverse('library:rented_list'))
         self.assertEqual(response.status_code, 200)
 
-    #def test_2(self):
+    def test_2(self):
+        response = self.client.get(reverse('library:rented_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Powrót do listy dostępnych książek.')
+
+    def test_3(self):
+        response = self.client.get(reverse('library:rented_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Brak dostępnych książek')
+        self.assertNotContains(response, 'Minęła data zwrotu.')
+
+    def test_4(self):
+        response = self.client.get(reverse('library:rented_list'))
+        self.assertEqual(response.status_code, 200)
+
